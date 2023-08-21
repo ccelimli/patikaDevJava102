98884c3ba7b1e84cb184399cdeaa8391a24f4e2a
Çağatay Çelimli <c.celimli@live.co.uk>
2023-08-21 16:26:42 +0300

    LinkedHashSet

--- collections/src/Main.java
+++ collections/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,18 @@ public class Main {
         hashSet.clear();
 
         Iterator<Integer> integerIterator= hashSet.iterator();
+
+        LinkedHashSet<Integer> integerLinkedHashSet=new LinkedHashSet<>();
+        integerLinkedHashSet.add(1);
+        integerLinkedHashSet.add(2);
+        integerLinkedHashSet.add(3);
+        integerLinkedHashSet.add(4);
+
+        Iterator<Integer> integerIterator1= integerLinkedHashSet.iterator();
+        while (integerIterator1.hasNext()){
+            System.out.println(integerIterator1.next());
+        }
+
 
     }
 }
