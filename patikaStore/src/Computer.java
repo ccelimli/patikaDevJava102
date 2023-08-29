5fd19ad6bb60bf91882c5210fc3b25eefc994b02
Çağatay Çelimli <c.celimli@live.co.uk>
2023-08-29 21:42:20 +0300

    Patika Store Update

--- patikaStore/src/Computer.java
+++ patikaStore/src/Computer.java
@@ -1,12 +1,9 @@
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Computer extends Product {
     private Scanner input = new Scanner(System.in);
-    private int computerId=1;
+    private int computerId = 1;
 
     public void setComputerId(int computerId) {
         this.computerId = computerId;
@@ -18,12 +15,14 @@ public class Computer extends Product {
 
     private List<Computer> computers = new ArrayList<>();
 
-    public Computer(){
+    public Computer() {
         super();
-    };
+    }
 
-    public Computer(Integer id, Integer unitPrice, Float discountRate, Integer amountStock, String productName, Brand brand, String memory, String screenSize, Integer ram) {
-        super(id, unitPrice, discountRate, amountStock, productName, brand, memory, screenSize, ram);
+    ;
+
+    public Computer(Integer id, Integer unitPrice, Float discountRate, Integer amountStock, String productName, String brandName, Integer memory, Float screenSize, Integer ram) {
+        super(id, unitPrice, discountRate, amountStock, productName, brandName, memory, screenSize, ram);
     }
 
     public void process() {
@@ -32,61 +31,128 @@ public class Computer extends Product {
             System.out.println("1 - Ekleme");
             System.out.println("2 - Silme");
             System.out.println("3 - Listeleme");
+            System.out.println("4 - Markaya Göre Arama");
+            System.out.println("5 - Id'ye Göre Arama");
+            System.out.println("6 - Geri Dön");
             int select = input.nextInt();
             switch (select) {
                 case 1 -> this.add();
                 case 2 -> this.delete();
                 case 3 -> this.displayComputers();
+                case 4 -> this.getByBrand();
+                case 5 -> this.getById();
+                case 6 -> Store.start();
                 default -> System.out.println("Yanlış Seçim");
             }
         }
     }
 
-    public void add(){
-        Computer computer=null;
+    private void getById() {
+        System.out.println("Ürüne ait ID giriniz: ");
+        Integer findId = input.nextInt();
+        for (Computer item : computers) {
+            if (Objects.equals(item.getId(), findId)) {
+                System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%.2f\t%d",
+                        item.getId(), item.getBrandName(), item.getProductName(),
+                        item.getRam(), item.getMemory(), item.getScreenSize(), item.getUnitPrice(),
+                        item.getDiscountRate(), item.getAmountStock());
+            } else {
+                System.out.println(findId + " numaralı ID'ye ait herhangi bir ürün bulunamadı!");
+            }
+        }
+    }
+
+    private void getByBrand() {
+        System.out.println("Marka İsmi Giriniz: ");
+        String brandName = input.nextLine();
+        for (Computer item : computers) {
+            if (Objects.equals(item.getBrandName(), brandName)) {
+                System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%.2f\t%d",
+                        item.getId(), item.getBrandName(), item.getProductName(),
+                        item.getRam(), item.getMemory(), item.getScreenSize(), item.getUnitPrice(),
+                        item.getDiscountRate(), item.getAmountStock());
+            } else {
+                System.out.println(brandName + " markasına ait herhangi bir ürün bulunamadı!");
+            }
+        }
+    }
+
+
+    public void add() {
+        Computer computer = new Computer();
         computer.setId(this.getComputerId());
 
         System.out.println("Ücret: ");
-        Integer unitPrice= input.nextInt();
+        Integer unitPrice = input.nextInt();
         computer.setUnitPrice(unitPrice);
 
+
         System.out.println("İndirim Oranı: ");
-        Float discountRate= input.nextFloat();
+        Float discountRate = input.nextFloat();
         computer.setDiscountRate(discountRate);
 
         System.out.println("Stok Adedi: ");
-        Integer amountStock= input.nextInt();
+        Integer amountStock = input.nextInt();
         computer.setAmountStock(amountStock);
 
         System.out.println("Ürün Adı: ");
-        String productName= input.nextLine();
+        String productName = input.nextLine();
         computer.setProductName(productName);
 
         System.out.println("Marka Seç: ");
         computer.setBrandName(new Brand().selectBrand());
 
         System.out.println("Hafıza: ");
-        Integer memory= input.nextInt();
+        Integer memory = input.nextInt();
         computer.setMemory(memory);
 
         System.out.println("Ekran Boyutu: ");
-        Float screenSize= input.nextFloat();
+        Float screenSize = input.nextFloat();
         computer.setScreenSize(screenSize);
 
         System.out.println("Ram: ");
-        Integer ram= input.nextInt();
+        Integer ram = input.nextInt();
         computer.setRam(ram);
 
         this.computers.add(computer);
-        this.setComputerId(this.getComputerId()+1);
+        System.out.println("Eklenen Ürün: ");
+        System.out.format("Id\tMarka\tÜrün Adı\tRam\tHafıza\tEkran Boyutu\tFiyat\tİndirim Oranı\tStok Adedi\n");
+        System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%.2f\t%d\n\n",
+                computer.getId(), computer.getBrandName(), computer.getProductName(),
+                computer.getRam(), computer.getMemory(), computer.getScreenSize(), computer.getUnitPrice(),
+                computer.getDiscountRate(), computer.getAmountStock());
+        this.setComputerId(this.getComputerId() + 1);
     }
 
-    public void delete(){
-        for (Computer computer : computers) {
+    public void delete() {
+        this.displayComputers();
+
+        System.out.println("Silmek istediğiniz ürün: ");
+        Integer selectId = input.nextInt();
+        Computer selectComputer = null;
+        for (Computer item : computers) {
+            if (item.getId() == selectId) {
+                selectComputer.setId(item.getId());
+                selectComputer.setBrandName(item.getBrandName());
+                selectComputer.setProductName(item.getProductName());
+                selectComputer.setRam(item.getRam());
+                selectComputer.setMemory(item.getMemory());
+                selectComputer.setScreenSize(item.getScreenSize());
+                selectComputer.setUnitPrice(item.getUnitPrice());
+                selectComputer.setDiscountRate(item.getDiscountRate());
+                selectComputer.setAmountStock(item.getAmountStock());
+            }
+        }
+        this.computers.remove(selectComputer);
+        this.displayComputers();
+    }
+
+    private void displayComputers() {
+        for (Computer item : computers) {
             System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%.2f\t%d",
-                    computer.getId(), computer.getBrandName(), computer.getProductName(),
-                    computer.getRam(), computer.getMemory(), computer.getScreenSize(),computer.getUnitPrice(),
-                    computer.getDiscountRate(), computer.getAmountStock());
+                    item.getId(), item.getBrandName(), item.getProductName(),
+                    item.getRam(), item.getMemory(), item.getScreenSize(), item.getUnitPrice(),
+                    item.getDiscountRate(), item.getAmountStock());
         }
     }
 }
--- patikaStore/src/SmartPhone.java
+++ patikaStore/src/SmartPhone.java
@@ -1,10 +1,184 @@
-public class SmartPhone extends Product{
-    int batterryPower;
-    String color;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
-    public SmartPhone(Integer id, Integer unitPrice, Float discountRate, Integer amountStock, String productName, Brand brand, String memory, String screenSize, Integer ram, int batterryPower, String color) {
-        super(id, unitPrice, discountRate, amountStock, productName, brand, memory, screenSize, ram);
-        this.batterryPower = batterryPower;
+public class SmartPhone extends Product{
+    private int batteryPower;
+    private String color;
+    private int smartPhoneId=1;
+    private List<SmartPhone> smartPhones= new ArrayList<>();
+    Scanner input= new Scanner(System.in);
+
+    public int getBatteryPower() {
+        return batteryPower;
+    }
+
+    public void setBatterryPower(int batterryPower) {
+        this.batteryPower = batterryPower;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
         this.color = color;
+    }
+
+    public int getSmartPhoneId() {
+        return smartPhoneId;
+    }
+
+    public void setSmartPhoneId(int smartPhoneId) {
+        this.smartPhoneId = smartPhoneId;
+    }
+
+    public SmartPhone(){
+      super();
+    }
+    public SmartPhone(Integer id, Integer unitPrice, Float discountRate, Integer amountStock, String productName, String brandName, Integer memory, Float screenSize, Integer ram, int batterryPower, String color) {
+        super(id, unitPrice, discountRate, amountStock, productName, brandName, memory, screenSize, ram);
+        this.batteryPower = batterryPower;
+        this.color = color;
+    }
+
+    public void process() {
+        while (true) {
+            System.out.println("İşlem Seç");
+            System.out.println("1 - Ekleme");
+            System.out.println("2 - Silme");
+            System.out.println("3 - Listeleme");
+            System.out.println("4 - Markaya Göre Arama");
+            System.out.println("5 - Id'ye Göre Arama");
+            System.out.println("6 - Geri Dön");
+            int select = input.nextInt();
+            switch (select) {
+                case 1 -> this.add();
+                case 2 -> this.delete();
+                case 3 -> this.displaySmartPhones();
+                case 4 -> this.getByBrand();
+                case 5 -> this.getById();
+                case 6 -> Store.start();
+                default -> System.out.println("Yanlış Seçim");
+            }
+        }
+    }
+
+    private void getById() {
+        System.out.println("Ürüne ait ID giriniz: ");
+        Integer findId = input.nextInt();
+        for (SmartPhone item : smartPhones) {
+            if (Objects.equals(item.getId(), findId)) {
+                System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%s\t%.2f\t%.2f\t%d",
+                        item.getId(), item.getBrandName(), item.getProductName(),
+                        item.getRam(), item.getMemory(), item.getScreenSize(),item.getBatteryPower(),item.getColor(), item.getUnitPrice(),
+                        item.getDiscountRate(), item.getAmountStock());
+            } else {
+                System.out.println(findId + " numaralı ID'ye ait herhangi bir ürün bulunamadı!");
+            }
+        }
+    }
+
+    private void getByBrand() {
+        System.out.println("Marka İsmi Giriniz: ");
+        String brandName = input.nextLine();
+        for (SmartPhone item : smartPhones) {
+            if (Objects.equals(item.getBrandName(), brandName)) {
+                System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%s\t%.2f\t%.2f\t%d",
+                        item.getId(), item.getBrandName(), item.getProductName(),
+                        item.getRam(), item.getMemory(), item.getScreenSize(),item.getBatteryPower(),item.getColor(), item.getUnitPrice(),
+                        item.getDiscountRate(), item.getAmountStock());
+            } else {
+                System.out.println(brandName + " markasına ait herhangi bir ürün bulunamadı!");
+            }
+        }
+    }
+
+    private void displaySmartPhones() {
+        for (SmartPhone item : smartPhones) {
+            System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%s\t%.2f\t%.2f\t%d",
+                    item.getId(), item.getBrandName(), item.getProductName(),
+                    item.getRam(), item.getMemory(), item.getScreenSize(),item.getBatteryPower(),item.getColor(), item.getUnitPrice(),
+                    item.getDiscountRate(), item.getAmountStock());
+        }
+    }
+
+    private void delete() {
+        this.displaySmartPhones();
+
+        System.out.println("Silmek istediğiniz ürün: ");
+        Integer selectId = input.nextInt();
+        SmartPhone selectSmartPhone = null;
+        for (SmartPhone item : smartPhones) {
+            if (item.getId() == selectId) {
+                selectSmartPhone.setId(item.getId());
+                selectSmartPhone.setBrandName(item.getBrandName());
+                selectSmartPhone.setProductName(item.getProductName());
+                selectSmartPhone.setRam(item.getRam());
+                selectSmartPhone.setMemory(item.getMemory());
+                selectSmartPhone.setScreenSize(item.getScreenSize());
+                selectSmartPhone.setUnitPrice(item.getUnitPrice());
+                selectSmartPhone.setColor(item.getColor());
+                selectSmartPhone.setBatterryPower(item.getBatteryPower());
+                selectSmartPhone.setDiscountRate(item.getDiscountRate());
+                selectSmartPhone.setAmountStock(item.getAmountStock());
+            }
+        }
+        this.smartPhones.remove(selectSmartPhone);
+        this.displaySmartPhones();
+    }
+
+    private void add() {
+        SmartPhone smartPhone = null;
+        smartPhone.setId(this.getSmartPhoneId());
+
+        System.out.println("Ücret: ");
+        Integer unitPrice = input.nextInt();
+        smartPhone.setUnitPrice(unitPrice);
+
+        System.out.println("İndirim Oranı: ");
+        Float discountRate = input.nextFloat();
+        smartPhone.setDiscountRate(discountRate);
+
+        System.out.println("Stok Adedi: ");
+        Integer amountStock = input.nextInt();
+        smartPhone.setAmountStock(amountStock);
+
+        System.out.println("Ürün Adı: ");
+        String productName = input.nextLine();
+        smartPhone.setProductName(productName);
+
+        System.out.println("Marka Seç: ");
+        smartPhone.setBrandName(new Brand().selectBrand());
+
+        System.out.println("Hafıza: ");
+        Integer memory = input.nextInt();
+        smartPhone.setMemory(memory);
+
+        System.out.println("Ekran Boyutu: ");
+        Float screenSize = input.nextFloat();
+        smartPhone.setScreenSize(screenSize);
+
+        System.out.println("Ram: ");
+        Integer ram = input.nextInt();
+        smartPhone.setRam(ram);
+
+        System.out.println("Batarya: ");
+        Integer batteryPower=input.nextInt();
+        smartPhone.setBatterryPower(batteryPower);
+
+        System.out.println("Renk: ");
+        String color=input.next();
+        smartPhone.setColor(color);
+
+        this.smartPhones.add(smartPhone);
+        System.out.println("Eklenen Ürün: ");
+        System.out.format("%d\t%s\t%s\t%d\t%d\t%.2f\t%d\t%s\t%.2f\t%.2f\t%d",
+                smartPhone.getId(), smartPhone.getBrandName(), smartPhone.getProductName(),
+                smartPhone.getRam(), smartPhone.getMemory(), smartPhone.getScreenSize(),smartPhone.getBatteryPower(),smartPhone.getColor(), smartPhone.getUnitPrice(),
+                smartPhone.getDiscountRate(), smartPhone.getAmountStock());
+        this.setSmartPhoneId(this.getSmartPhoneId() + 1);
     }
 }
