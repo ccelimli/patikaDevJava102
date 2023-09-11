98e73f2bb9946a0742c38dfb356b1ed9218bc3aa
Çağatay Çelimli <c.celimli@live.co.uk>
2023-09-11 03:59:53 +0300

    Thread

---
+++ thread/src/Counter.java
@@ -0,0 +1,16 @@
+public class Counter extends Thread{
+    public String name;
+
+    public Counter(String name){
+        this.name= name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(this.name+" Çalıştı!");
+
+        for (int i=0; i<100; i++){
+            System.out.println(this.name+": "+i);
+        }
+    }
+}
